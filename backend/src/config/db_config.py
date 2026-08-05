e817f05e8fcc4908074f7c3e218737ffc0470526
Mionuel <lifevlad2015@gmail.com>
2026-08-05 13:32:51 +0200

    feat: moved the schema into .sql file. the sql script will now be automatically executed on first run

--- backend/src/config/db_config.py
+++ backend/src/config/db_config.py
@@ -8,6 +8,15 @@ db_config = {
     "password": "pass"
 }
 
+# Establishes a connection with the db
 def get_db():
     with psycopg.connect(**db_config, row_factory=dict_row) as conn:
-        yield conn    
+        yield conn   
+
+# Reads a .sql file as a string and runs it wia psycopg
+def run_sql(file_path: str):
+    with open(file_path) as f:
+        sql_script = f.read()
+
+    with psycopg.connect(**db_config) as conn:
+        conn.execute(sql_script)
