0fbae455e9c6bfa9113dc39471e831c55a0c5e9f
Mionuel <lifevlad2015@gmail.com>
2026-08-05 16:54:56 +0200

    feat: fetch person by id endpoint, consistency fixes

--- backend/src/api/controller/person_controller.py
+++ backend/src/api/controller/person_controller.py
@@ -1,11 +1,10 @@
 from typing import List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
 from config.db_config import get_db
 
-from api.view.person_view import new_person
-from api.view.person_view import all_people
+from api.view.person_view import new_person, get_person_by_id, all_people
 from api.model.person_model import PersonCreate, PersonResponse
 
 router = APIRouter(
@@ -13,17 +12,36 @@ router = APIRouter(
     tags=["People"]
 )
 
-@router.get("/all", response_model=List[PersonResponse])
+@router.get("/", response_model=List[PersonResponse])
 def fetch_all_people(db=Depends(get_db)):
+    """Fetches all people"""
     people = db.execute(
         all_people
     ).fetchall()
     return people
 
-@router.post("/new")
+@router.post("/", response_model=PersonResponse)
 def create_person(person: PersonCreate, db=Depends(get_db)):
     """Creates a new person"""
-    db.execute(
+    created_person = db.execute(
         new_person, 
         (person.name, person.init_credit, person.init_bonus)
-    )
+    ).fetchone()
+
+    if created_person is None:
+        raise HTTPException(status_code=400, detail="Could not create person")
+
+    return created_person
+
+@router.get("/{person_id}", response_model=PersonResponse)
+def fetch_person(person_id: int, db=Depends(get_db)):
+    """Fetches a person with id=person_id"""
+    person = db.execute(
+        get_person_by_id,
+        [person_id]
+    ).fetchone()
+
+    if not person:
+        raise HTTPException(status_code=404, detail="Person not found")
+
+    return person
--- backend/src/api/view/person_view.py
+++ backend/src/api/view/person_view.py
@@ -1,8 +1,15 @@
+# RETURNING returns the newly created person's data
 new_person = """
-    INSERT INTO Person (name, credit) 
-    VALUES (%s, %s)
+    INSERT INTO "Person" (name, credit, bonus_points) 
+    VALUES (%s, %s, %s)
+    RETURNING id, name, credit, bonus_points;
 """
 
 all_people = """
-    SELECT * FROM Person;
+    SELECT * FROM "Person";
+"""
+
+get_person_by_id = """
+    SELECT * FROM "Person" 
+    WHERE id = %s;
 """
