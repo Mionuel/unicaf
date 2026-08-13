93b233d42f0fdbe73ce4158d3a895a14629653e9
Mionuel <lifevlad2015@gmail.com>
2026-08-13 13:56:00 +0200

    fix: removed the unused order functionality

--- backend/src/api/controller/order_controller.py
+++
@@ -1,6 +0,0 @@
-from api.view.order_view import insert_order_sql 
-
-ORDER_COST = 10.0
-
-def insert_order(person_id: int, seat_id: int, bonus_snack: bool, db):
-    db.execute(insert_order_sql, (person_id, seat_id, ORDER_COST, bonus_snack))
--- backend/src/api/view/order_view.py
+++
@@ -1,4 +0,0 @@
-insert_order_sql = """
-    INSERT INTO "Order" (person_id, seat_id, cost, bonus_snack)
-    VALUES (%s, %s, %s, %s)
-"""
