c1bf129743b8158c9db362277bad4b1d7b9a131a
Mionuel <lifevlad2015@gmail.com>
2026-08-20 16:25:26 +0200

    fix: now everything is deeefinitely in vanilla js

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,6 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />,
 );
